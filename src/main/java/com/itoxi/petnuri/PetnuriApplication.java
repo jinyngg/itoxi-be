9a1ed1ccaad33ff196124922a97b40a281922281
jyjang <jinyngg@gmail.com>
2023-09-04 13:08:32 +0900

    init : 프로젝트 초기화

---
+++ src/main/java/com/itoxi/petnuri/PetnuriApplication.java
@@ -0,0 +1,13 @@
+package com.itoxi.petnuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetnuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetnuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itoxi/petnuri/PetnuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itoxi.petnuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetnuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
