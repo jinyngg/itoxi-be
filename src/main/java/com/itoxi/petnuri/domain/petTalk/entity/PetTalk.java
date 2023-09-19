1de473898eb9e9d5aaa8ce2b528136641c4e31d1
chaewon <djacltk@naver.com>
2023-09-19 19:37:23 +0900

    [#12] feat : 펫톡 Entity conflict 해결

--- src/main/java/com/itoxi/petnuri/domain/petTalk/entity/PetTalk.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/entity/PetTalk.java
@@ -1,33 +1,19 @@
 package com.itoxi.petnuri.domain.petTalk.entity;
 
-import static com.itoxi.petnuri.domain.petTalk.type.PetTalkStatus.ACTIVE;
-
 import com.itoxi.petnuri.domain.member.entity.Member;
 import com.itoxi.petnuri.domain.petTalk.type.PetTalkStatus;
 import com.itoxi.petnuri.domain.petTalk.type.PetType;
 import com.itoxi.petnuri.global.common.BaseTimeEntity;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderBy;
-import javax.persistence.Table;
-import javax.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.itoxi.petnuri.domain.petTalk.type.PetTalkStatus.ACTIVE;
 
 @Getter
 @Builder
@@ -56,7 +42,7 @@ public class PetTalk extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "sub_category_id")
     private SubCategory subCategory;
-    
+
     @Column(name = "pet_type", nullable = false)
     @Enumerated(EnumType.STRING)
     private PetType petType;
