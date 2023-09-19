581a1fb61bdba5f2cf93ab519bffb04b1dcbb56e
jyjang <jinyngg@gmail.com>
2023-09-19 13:11:42 +0900

    [#21] feat : BaseTimeEntity 시간 포맷 추가

--- src/main/java/com/itoxi/petnuri/global/common/BaseTimeEntity.java
+++ src/main/java/com/itoxi/petnuri/global/common/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.itoxi.petnuri.global.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
@@ -14,9 +15,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class BaseTimeEntity {
 
     @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime updatedAt;
 
 }
