4b441303e43a8cabe02ba7dc1547fc97529fce9c
jyjang <jinyngg@gmail.com>
2023-09-19 13:10:30 +0900

    [#21] feat : 펫톡 Repository 추가 conflict 해결
    1. 전체 조회를 위한 QueryDSL 메소드 작성
    2. 게시글 작성 메소드 추가

--- src/main/java/com/itoxi/petnuri/domain/petTalk/repository/PetTalkJpaRepository.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/repository/PetTalkJpaRepository.java
@@ -1,8 +1,8 @@
 package com.itoxi.petnuri.domain.petTalk.repository;
 
-import com.itoxi.petnuri.domain.petTalk.entity.PetTalk;
+import com.itoxi.petnuri.domain.petTalk.entity.PetTalkPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetTalkJpaRepository extends JpaRepository<PetTalk, Long>, PetTalkJpaRepositoryCustom {
+public interface PetTalkJpaRepository extends JpaRepository<PetTalkPost, Long>, PetTalkJpaRepositoryCustom {
 
 }
