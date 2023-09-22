601dea0694d5f94d0cd40c25fe6462248f15dcd7
jyjang <jinyngg@gmail.com>
2023-09-22 19:19:13 +0900

    [#63] feat : Repo 메소드 추가
    1. 상세 조회 로직 추가
    2. 레디스를 통한 조회수 업데이트 로직 추가
    3. BooleanExpression 관리

--- src/main/java/com/itoxi/petnuri/domain/petTalk/repository/impl/PetTalkJpaRepositoryCustomImpl.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/repository/impl/PetTalkJpaRepositoryCustomImpl.java
@@ -8,8 +8,11 @@ import com.itoxi.petnuri.domain.petTalk.repository.PetTalkJpaRepositoryCustom;
 import com.itoxi.petnuri.domain.petTalk.type.PetType;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +23,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class PetTalkJpaRepositoryCustomImpl implements PetTalkJpaRepositoryCustom {
+
+    private static final int RECENT_DAYS = 3;
 
     private final JPAQueryFactory queryFactory;
 
@@ -32,9 +37,10 @@ public class PetTalkJpaRepositoryCustomImpl implements PetTalkJpaRepositoryCusto
 
         List<PetTalk> petTalks = queryFactory
                 .selectFrom(qPetTalk)
-                .where(isStatusActive()
-                        .and(qPetTalk.mainCategory.id.eq(mainCategoryId))
-                        .and(qPetTalk.petType.eq(petType)))
+                .where(eqActive()
+                        .and(eqMainCategory(mainCategoryId))
+                        .and(eqSubCategory(subCategoryId))
+                        .and(eqPetType(petType)))
                 .orderBy(qPetTalk.updatedAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -52,22 +58,88 @@ public class PetTalkJpaRepositoryCustomImpl implements PetTalkJpaRepositoryCusto
 
         List<PetTalk> petTalks = queryFactory
                 .selectFrom(qPetTalk)
-                .where(isStatusActive()
-                        .and(qPetTalk.createdAt.after(LocalDateTime.now().minusDays(3)))
-                        .and(qPetTalk.petType.eq(petType)))
-                .orderBy(
-                        qPetTalk.viewCount.desc(),
-                        qPetTalk.updatedAt.desc())
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
+                .where(eqActive()
+                        .and(recentlyCreated())
+                        .and(eqPetType(petType)))
                 .fetch();
+
+        // Ranking Algorithm
+        List<PetTalk> sortedPetTalks = petTalks.stream()
+                .sorted((current, next) -> {
+                    Long currentEmojiCount = current.getEmojiCount();
+                    Long nextEmojiCount = next.getEmojiCount();
+
+                    LocalDateTime currentCreatedAt = current.getCreatedAt();
+                    LocalDateTime nextCreatedAt = next.getCreatedAt();
+
+                    long currentHoursElapsed = Duration.between(currentCreatedAt, LocalDateTime.now()).toHours();
+                    long nextHoursElapsed = Duration.between(nextCreatedAt, LocalDateTime.now()).toHours();
+
+                    double currentScore = (currentEmojiCount - 1) / Math.pow(currentHoursElapsed + 2, 2);
+                    double nextScore = (nextEmojiCount - 1) / Math.pow(nextHoursElapsed + 2, 2);
+
+                    return Double.compare(nextScore, currentScore);
+                })
+                .collect(Collectors.toList());
+
+        // Paging
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), sortedPetTalks.size());
+        List<PetTalk> pagePetTalks = sortedPetTalks.subList(start, end);
 
         long totalCount = petTalks.size();
 
-        return PageableExecutionUtils.getPage(petTalks, pageable, () -> totalCount);
+        return PageableExecutionUtils.getPage(pagePetTalks, pageable, () -> totalCount);
     }
 
-    private BooleanExpression isStatusActive() {
+    @Override
+    public Optional<PetTalk> loadPetTalkPostsDetails(Long petTalkId) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(qPetTalk)
+                        .where(eqActive())
+                        .fetchOne());
+    }
+
+    @Override
+    public void addViewCountFromRedis(Long petTalkId, Long petTalkViewCount) {
+        queryFactory
+                .update(qPetTalk)
+                .set(qPetTalk.viewCount, petTalkViewCount)
+                .where(eqPetTalkId(petTalkId))
+                .execute();
+    }
+
+
+    private BooleanExpression eqPetTalkId(Long petTalkId) {
+        return qPetTalk.id.eq(petTalkId);
+    }
+
+    private BooleanExpression recentlyCreated() {
+        return qPetTalk.createdAt.after(LocalDateTime.now().minusDays(RECENT_DAYS));
+    }
+
+    private BooleanExpression eqActive() {
         return qPetTalk.status.eq(ACTIVE);
+    }
+
+    private BooleanExpression eqPetType(PetType petType) {
+        return qPetTalk.petType.eq(petType);
+    }
+
+    private BooleanExpression eqMainCategory(Long mainCategoryId) {
+        if (mainCategoryId == null) {
+            return null;
+        }
+
+        return qPetTalk.mainCategory.id.eq(mainCategoryId);
+    }
+
+    private BooleanExpression eqSubCategory(Long subCategoryId) {
+        if (subCategoryId == null) {
+            return null;
+        }
+
+        return qPetTalk.subCategory.id.eq(subCategoryId);
     }
 }
