0f6fa368829c7838a5a4c1db479dd11dfa8ab361
jyjang <jinyngg@gmail.com>
2023-09-22 19:19:27 +0900

    [#63] feat : 카테고리 관련 JPA Repo 추가

---
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/repository/MainCategoryJpaRepository.java
@@ -0,0 +1,8 @@
+package com.itoxi.petnuri.domain.petTalk.repository;
+
+import com.itoxi.petnuri.domain.petTalk.entity.MainCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MainCategoryJpaRepository extends JpaRepository<MainCategory, Long> {
+
+}
---
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/repository/SubCategoryJpaRepository.java
@@ -0,0 +1,8 @@
+package com.itoxi.petnuri.domain.petTalk.repository;
+
+import com.itoxi.petnuri.domain.petTalk.entity.SubCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubCategoryJpaRepository extends JpaRepository<SubCategory, Long> {
+
+}
