5ac85403335b92f5cc8b5283d61fe250a522c00c
jyjang <jinyngg@gmail.com>
2023-09-25 01:55:40 +0900

    [#78] fix : RedisConfig 수정

--- src/main/java/com/itoxi/petnuri/global/config/RedisConfig.java
+++ src/main/java/com/itoxi/petnuri/global/config/RedisConfig.java
@@ -18,20 +18,20 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.password}")
-    private String redisPassword;
+//    @Value("${spring.redis.password}")
+//    private String redisPassword;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
-        redisConfig.setPassword(redisPassword);
+//        redisConfig.setPassword(redisPassword);
         return new LettuceConnectionFactory(redisConfig);
     }
     
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setEnableTransactionSupport(true);
+//        redisTemplate.setEnableTransactionSupport(true);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
