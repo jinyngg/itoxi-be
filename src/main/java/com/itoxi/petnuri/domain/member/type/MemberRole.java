8574810c69603ba753ccf81b827e6e69e15b3b97
inyoung <charmingyear@gmail.com>
2023-09-27 14:08:46 +0900

    [#116] fix : enum 클래스 수정

--- src/main/java/com/itoxi/petnuri/domain/member/type/MemberRole.java
+++ src/main/java/com/itoxi/petnuri/domain/member/type/MemberRole.java
@@ -1,9 +1,17 @@
 package com.itoxi.petnuri.domain.member.type;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public enum MemberRole {
 
-    USER, ADMIN
+    ADMIN("ROLE_ADMIN", "어드민")
+    , USER("ROLE_USER", "유저")
+
+    ;
+
+    private final String role;
+    private final String description;
 }
