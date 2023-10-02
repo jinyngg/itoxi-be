768009a943b95824d2502eebed920a32627603c6
jyjang <jinyngg@gmail.com>
2023-10-03 01:45:43 +0900

    [#130] feat : 레포 추가 작성

--- src/main/java/com/itoxi/petnuri/domain/eventChallenge/repository/PointChallengeRepository.java
+++ src/main/java/com/itoxi/petnuri/domain/eventChallenge/repository/PointChallengeRepository.java
@@ -1,10 +1,13 @@
 package com.itoxi.petnuri.domain.eventChallenge.repository;
 
-import static com.itoxi.petnuri.global.common.exception.type.ErrorCode.NOT_FOUND_EVENT_CHALLENGE_ID;
 import static com.itoxi.petnuri.domain.eventChallenge.type.EventChallengeType.POINT;
+import static com.itoxi.petnuri.global.common.exception.type.ErrorCode.NOT_FOUND_EVENT_CHALLENGE_ID;
+import static com.itoxi.petnuri.global.common.exception.type.ErrorCode.NOT_FOUND_MEMBER_REVIEW;
 
 import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallenge;
 import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallengeReview;
+import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallengeReward;
+import com.itoxi.petnuri.domain.eventChallenge.type.PointChallengeStatus;
 import com.itoxi.petnuri.domain.member.entity.Member;
 import com.itoxi.petnuri.global.common.exception.Exception400;
 import com.itoxi.petnuri.global.s3.service.AmazonS3Service;
@@ -18,16 +21,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class PointChallengeRepository {
 
     private final AmazonS3Service amazonS3Service;
-    private final PointChallengeJpaJpaRepository pointChallengeJPARepository;
-    private final PointChallengeReviewReviewJpaRepository pointChallengeReviewJpaRepository;
+    private final PointChallengeJpaRepository pointChallengeJpaRepository;
+    private final PointChallengeReviewJpaRepository pointChallengeReviewJpaRepository;
+    private final PointChallengeRewardJpaRepository pointChallengeRewardJpaRepository;
 
     public PointChallenge getPointChallengeById(Long pointChallengeId) {
-        return pointChallengeJPARepository.findById(pointChallengeId)
+        return pointChallengeJpaRepository.findById(pointChallengeId)
                 .orElseThrow(() -> new Exception400(NOT_FOUND_EVENT_CHALLENGE_ID));
     }
 
     public void writePointChallengePost(PointChallenge pointChallenge) {
-        pointChallengeJPARepository.save(pointChallenge);
+        pointChallengeJpaRepository.save(pointChallenge);
     }
 
     public void writePointChallengeReview(PointChallengeReview pointChallengeReview) {
@@ -66,7 +70,44 @@ public class PointChallengeRepository {
                         pointChallenge, challenger);
     }
 
-    public void uploadReviewPhoto(MultipartFile photo, PointChallengeReview pointChallengeReview) {
+    public boolean isWritePointChallengeReviewToday(PointChallenge pointChallenge, Member member) {
+        return pointChallengeReviewJpaRepository.existsTodayReviewByPointChallengeAndReviewer(
+                pointChallenge, member);
+    }
 
+    public PointChallengeReview getLatestPointChallengeReviewByPointChallengeAndMember(
+            PointChallenge pointChallenge, Member member) {
+        return pointChallengeReviewJpaRepository.findLatestPointChallengeReviewByPointChallengeAndMember(pointChallenge, member)
+                .orElseThrow(() -> new Exception400(NOT_FOUND_MEMBER_REVIEW));
+    }
+
+    public List<PointChallenge> getPointChallengeByStatus(PointChallengeStatus status) {
+        return pointChallengeJpaRepository.findAllByStatus(status);
+    }
+
+    public List<PointChallenge> getPointChallengeExceptClosed() {
+        return pointChallengeJpaRepository.findAllExceptClosed();
+    }
+
+    public void updatePointChallenges(List<PointChallenge> pointChallenges) {
+        if (!pointChallenges.isEmpty()) {
+            pointChallengeJpaRepository.saveAll(pointChallenges);
+        }
+    }
+
+    public List<Member> getMembersForPointReward(PointChallenge pointChallenge, Long reviewCount) {
+        return pointChallengeReviewJpaRepository.findMembersForPointReward(
+                pointChallenge, reviewCount);
+    }
+
+    public List<PointChallengeReward> getPointChallengesRewardByPointChallenge(
+            PointChallenge pointChallenge) {
+        return pointChallengeRewardJpaRepository.findAllByPointChallenge(pointChallenge);
+    }
+
+    public void updatePointChallengeRewards(List<PointChallengeReward> pointChallengeRewards) {
+        if (!pointChallengeRewards.isEmpty()) {
+            pointChallengeRewardJpaRepository.saveAll(pointChallengeRewards);
+        }
     }
 }
