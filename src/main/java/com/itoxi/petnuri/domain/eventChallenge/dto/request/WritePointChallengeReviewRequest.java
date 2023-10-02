5a8443332363785cc5bf3d93adbf9ab57260a5a7
jyjang <jinyngg@gmail.com>
2023-10-03 01:39:06 +0900

    [#130] feat : 리뷰 작성시 PetType 추가

--- src/main/java/com/itoxi/petnuri/domain/eventChallenge/dto/request/WritePointChallengeReviewRequest.java
+++ src/main/java/com/itoxi/petnuri/domain/eventChallenge/dto/request/WritePointChallengeReviewRequest.java
@@ -1,10 +1,12 @@
 package com.itoxi.petnuri.domain.eventChallenge.dto.request;
 
+import com.itoxi.petnuri.domain.petTalk.type.PetType;
 import lombok.Getter;
 
 @Getter
 public class WritePointChallengeReviewRequest {
 
+    private PetType petType;
     private String content;
 
 }
