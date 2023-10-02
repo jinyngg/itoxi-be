4ddd3341cf7c3863adacba6c41332e04653d2377
jyjang <jinyngg@gmail.com>
2023-10-03 01:42:37 +0900

    [#130] feat : 종료되지 않은 챌린지를 조회하는 로직 추가

---
+++ src/main/java/com/itoxi/petnuri/domain/eventChallenge/repository/PointChallengeJpaRepository.java
@@ -0,0 +1,16 @@
+package com.itoxi.petnuri.domain.eventChallenge.repository;
+
+import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallenge;
+import com.itoxi.petnuri.domain.eventChallenge.type.PointChallengeStatus;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PointChallengeJpaRepository extends JpaRepository<PointChallenge, Long> {
+
+    @Query("SELECT challenge FROM PointChallenge challenge WHERE challenge.status != 'CLOSED'")
+    List<PointChallenge> findAllExceptClosed();
+
+    List<PointChallenge> findAllByStatus(PointChallengeStatus status);
+
+}
