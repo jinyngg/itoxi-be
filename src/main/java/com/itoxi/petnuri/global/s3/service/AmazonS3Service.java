879804e662ccd0d108b8647a636756649834655d
jyjang <jinyngg@gmail.com>
2023-10-03 01:46:45 +0900

    [#130] feat : File 업로드 로직 추가 작성

--- src/main/java/com/itoxi/petnuri/global/s3/service/AmazonS3Service.java
+++ src/main/java/com/itoxi/petnuri/global/s3/service/AmazonS3Service.java
@@ -1,26 +1,26 @@
 package com.itoxi.petnuri.global.s3.service;
 
+import static com.itoxi.petnuri.domain.eventChallenge.type.EventChallengeType.POINT;
+
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.itoxi.petnuri.domain.eventChallenge.type.EventChallengeType;
 import com.itoxi.petnuri.domain.petTalk.entity.PetTalk;
 import com.itoxi.petnuri.domain.petTalk.entity.PetTalkPhoto;
 import com.itoxi.petnuri.global.common.exception.Exception500;
 import com.itoxi.petnuri.global.common.exception.type.ErrorCode;
+import com.itoxi.petnuri.domain.eventChallenge.type.EventChallengeType;
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import static com.itoxi.petnuri.domain.eventChallenge.type.EventChallengeType.POINT;
 
 @Slf4j
 @Service
@@ -56,6 +56,9 @@ public class AmazonS3Service {
     private static final String EVENT_CHALLENGE_REWARD_REVIEW_PREFIX =
             "event_challenge/reward/review/";
 
+    private static final String EVENT_CHALLENGE_POINT_CSV_PREFIX = "event_challenge/point/csv/";
+    private static final String EVENT_CHALLENGE_REWARD_CSV_PREFIX = "event_challenge/reward/csv/";
+
     @Transactional
     public List<PetTalkPhoto> uploadPetTalkPhotos(MultipartFile[] files, PetTalk petTalk) {
         List<PetTalkPhoto> petTalkPhotos = new ArrayList<>();
@@ -65,7 +68,7 @@ public class AmazonS3Service {
             for (MultipartFile file : files) {
                 petTalkPhotos.add(PetTalkPhoto.builder()
                         .name(file.getOriginalFilename())
-                        .url(uploadImage(PET_TALK_PHOTO_PREFIX, file))
+                        .url(uploadMultipartFileToBucket(PET_TALK_PHOTO_PREFIX, file))
                         .petTalk(petTalk)
                         .build());
             }
@@ -78,64 +81,77 @@ public class AmazonS3Service {
         return petTalkPhotos;
     }
 
-    @Transactional
+    public String uploadPointChallengeCSV(File file) {
+        return uploadFileToBucket(EVENT_CHALLENGE_POINT_CSV_PREFIX, file);
+    }
+
     public String uploadRewardChallengeReviewImage(MultipartFile file) {
-        return uploadImage(EVENT_CHALLENGE_REWARD_REVIEW_PREFIX, file);
+        return uploadMultipartFileToBucket(EVENT_CHALLENGE_REWARD_REVIEW_PREFIX, file);
     }
 
     public String uploadPointChallengeReviewPhoto(MultipartFile file) {
-        return uploadImage(EVENT_CHALLENGE_POINT_REVIEW_PREFIX, file);
+        return uploadMultipartFileToBucket(EVENT_CHALLENGE_POINT_REVIEW_PREFIX, file);
     }
 
     public String uploadEventChallengeThumbnail(EventChallengeType type, MultipartFile thumbnail) {
         String path = (type == POINT) ?
                 EVENT_CHALLENGE_POINT_THUMBNAIL_PREFIX : EVENT_CHALLENGE_REWARD_THUMBNAIL_PREFIX;
 
-        return uploadImage(path, thumbnail);
+        return uploadMultipartFileToBucket(path, thumbnail);
     }
 
     public String uploadEventChallengePoster(EventChallengeType type, MultipartFile poster) {
         String path = (type == POINT) ?
                 EVENT_CHALLENGE_POINT_POSTER_PREFIX : EVENT_CHALLENGE_REWARD_POSTER_PREFIX;
 
-        return uploadImage(path, poster);
+        return uploadMultipartFileToBucket(path, poster);
     }
 
     // 프로필 이미지 수정
     public String uploadProfileImage(MultipartFile file) {
-        return uploadImage(MEMBER_IMAGE_PREFIX, file);
+        return uploadMultipartFileToBucket(MEMBER_IMAGE_PREFIX, file);
     }
 
     //펫 프로필 이미지 저장
     public String uploadPetProfileImage(MultipartFile image){
-        return uploadImage(PET_IMAGE_PREFIX, image);
+        return uploadMultipartFileToBucket(PET_IMAGE_PREFIX, image);
     }
 
     // 데일리 챌린지 인증 사진 저장
     public String uploadDailyChallengeAuthImage(MultipartFile file) {
-        return uploadImage(DAILY_CHALLENGE_AUTH_IMAGE_PREFIX, file);
+        return uploadMultipartFileToBucket(DAILY_CHALLENGE_AUTH_IMAGE_PREFIX, file);
     }
 
     // 데일리 챌린지 썸네일 저장
     public String uploadThumbnailImage(MultipartFile thumbnail) {
-        return uploadImage(DAILY_CHALLENGE_THUMBNAIL_PREFIX, thumbnail);
+        return uploadMultipartFileToBucket(DAILY_CHALLENGE_THUMBNAIL_PREFIX, thumbnail);
     }
 
     // 데일리 챌린지 메인 배너 저장
     public String uploadBannerImage(MultipartFile banner) {
-        return uploadImage(DAILY_CHALLENGE_BANNER_PREFIX, banner);
+        return uploadMultipartFileToBucket(DAILY_CHALLENGE_BANNER_PREFIX, banner);
     }
 
     // 단일 파일 저장
-    private String uploadImage(String subject, MultipartFile file) {
-        String fileKey = subject + createDatePath() + generateRandomFileName();
+    private String uploadMultipartFileToBucket(String subject, MultipartFile file) {
+        String filePath = subject + createDatePath() + generateRandomFileName();
         ObjectMetadata metadata = createObjectMetadataFromFile(file);
         try {
-            amazonS3.putObject(bucket, fileKey, file.getInputStream(), metadata);
+            amazonS3.putObject(bucket, filePath, file.getInputStream(), metadata);
         } catch (Exception e) {
             throw new Exception500(ErrorCode.S3UPLOADER_ERROR);
         }
-        return getUrlFromBucket(fileKey);
+        return getUrlFromBucket(filePath);
+    }
+
+    private String uploadFileToBucket(String subject, File file) {
+        String filePath = subject + createDatePath() + generateRandomFileName();
+        try {
+            amazonS3.putObject(bucket, filePath, file);
+        } catch (Exception e) {
+            throw new Exception500(ErrorCode.S3UPLOADER_ERROR);
+        }
+        return getUrlFromBucket(filePath);
     }
 
     private String generateRandomFileName() {
@@ -159,4 +175,15 @@ public class AmazonS3Service {
         return amazonS3.getUrl(bucket, fileKey).toString();
     }
 
+    // S3 deleteObject()의 filename 생성용
+    private String getFileNameFromUrl(String url) {
+        // 1. amazonaws.com 이 위치한 문자열 index를 가져온다.
+        int startIndex = url.indexOf(".com");
+
+        // 2. startIndex에서부터 처음 나오는 "/"의 위치 다움 문자 인덱스를 가져 온다.
+        int fromIndex = url.indexOf("/", startIndex) + 1;
+
+        // 3. fromIndex부터 끝까지 문자열을 잘라서 filename을 만들어서 반환.
+        return url.substring(fromIndex, url.length());
+    }
 }
