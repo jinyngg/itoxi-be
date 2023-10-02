f905efed668c5bb9ec6f367d016c1a20135e79ee
jyjang <jinyngg@gmail.com>
2023-10-03 01:45:36 +0900

    [#130] feat : 포인트 챌린지를 통해 지급 내역 목록 조회 로직 추가

---
+++ src/main/java/com/itoxi/petnuri/domain/eventChallenge/repository/PointChallengeRewardJpaRepository.java
@@ -0,0 +1,14 @@
+package com.itoxi.petnuri.domain.eventChallenge.repository;
+
+import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallenge;
+import com.itoxi.petnuri.domain.eventChallenge.entity.PointChallengeReward;
+import java.util.List;
+import java.util.Optional;
+import javax.swing.text.html.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointChallengeRewardJpaRepository extends JpaRepository<PointChallengeReward, Long> {
+
+    List<PointChallengeReward> findAllByPointChallenge(PointChallenge pointChallenge);
+
+}
