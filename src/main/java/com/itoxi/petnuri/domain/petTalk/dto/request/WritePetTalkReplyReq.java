0c8e2607cff8156836663dda7ed7f6e4fa4d2f38
chaewon <djacltk@naver.com>
2023-10-05 17:01:41 +0900

    [#166] fix: 댓글 글자수 유효성 검사 수정

--- src/main/java/com/itoxi/petnuri/domain/petTalk/dto/request/WritePetTalkReplyReq.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/dto/request/WritePetTalkReplyReq.java
@@ -2,14 +2,14 @@ package com.itoxi.petnuri.domain.petTalk.dto.request;
 
 import lombok.Getter;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 
 @Getter
 public class WritePetTalkReplyReq {
     @PositiveOrZero
     private Long parentId;
 
-    @Max(500)
+    @Size(max = 500, message = "글자수는 500자 이하여야 합니다.")
     private String content;
 }
