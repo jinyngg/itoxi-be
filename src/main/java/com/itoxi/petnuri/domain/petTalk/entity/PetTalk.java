572dd34c60b68b02ed8da2cbbedb41cd28c1022e
inyoung <charmingyear@gmail.com>
2023-10-05 21:39:33 +0900

    [#60] chore : 사용하지 않는 import문 제거

--- src/main/java/com/itoxi/petnuri/domain/petTalk/entity/PetTalk.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/entity/PetTalk.java
@@ -8,8 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.util.ArrayList;
--- src/main/java/com/itoxi/petnuri/domain/petTalk/repository/PetTalkRepository.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/repository/PetTalkRepository.java
@@ -20,7 +20,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
