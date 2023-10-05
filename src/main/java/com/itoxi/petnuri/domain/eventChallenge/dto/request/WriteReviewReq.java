99ee08fc540930d8b7a6527852a5c00a2d59f846
jyjang <jinyngg@gmail.com>
2023-10-06 02:00:18 +0900

    [#174] feat : 리워드 챌린지 컨텐트 예외처리 주석 처리

--- src/main/java/com/itoxi/petnuri/domain/eventChallenge/dto/request/WriteReviewReq.java
+++ src/main/java/com/itoxi/petnuri/domain/eventChallenge/dto/request/WriteReviewReq.java
@@ -4,13 +4,11 @@ import com.itoxi.petnuri.domain.petTalk.type.PetType;
 import com.itoxi.petnuri.global.common.customValid.valid.ValidEnum;
 import lombok.Getter;
 
-import javax.validation.constraints.Size;
-
 @Getter
 public class WriteReviewReq {
     @ValidEnum(enumClass = PetType.class)
     private PetType petType;
 
-    @Size(min = 15, max = 500, message = "글자수는 15~500자 이어야 합니다.")
+//    @Size(min = 15, max = 500, message = "글자수는 15~500자 이어야 합니다.")
     private String content;
 }
