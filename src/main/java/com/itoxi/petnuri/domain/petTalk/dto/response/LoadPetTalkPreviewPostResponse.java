1adb14777ccd300b29b25ec5ad43b626803448d5
jyjang <jinyngg@gmail.com>
2023-10-05 13:08:53 +0900

    [#156] feat : 펫톡 목록 조회시 PetTalkView 를 통해 조회

--- src/main/java/com/itoxi/petnuri/domain/petTalk/dto/response/LoadPetTalkPreviewPostResponse.java
+++ src/main/java/com/itoxi/petnuri/domain/petTalk/dto/response/LoadPetTalkPreviewPostResponse.java
@@ -1,9 +1,14 @@
 package com.itoxi.petnuri.domain.petTalk.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.itoxi.petnuri.domain.member.dto.Writer;
+import com.itoxi.petnuri.domain.petTalk.dto.EmojiCount;
 import com.itoxi.petnuri.domain.petTalk.entity.PetTalk;
+import com.itoxi.petnuri.domain.petTalk.entity.PetTalkView;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
@@ -16,24 +21,28 @@ public class LoadPetTalkPreviewPostResponse {
     private String thumbnail;
 
     private Long viewCount;
-    private Long emojiCount;
     private Long replyCount;
 
     private boolean reacted;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
+    private Long totalEmojiCount;
     private Writer writer;
 
-    public static LoadPetTalkPreviewPostResponse fromEntity(PetTalk petTalk) {
+    public static LoadPetTalkPreviewPostResponse fromEntity(PetTalkView petTalkView) {
         return LoadPetTalkPreviewPostResponse.builder()
-                .id(petTalk.getId())
-                .title(petTalk.getTitle())
-                .content(petTalk.getContent())
-                .thumbnail(petTalk.getThumbnail())
-                .viewCount(petTalk.getViewCount())
-                .emojiCount(petTalk.getEmojiCount())
-                .replyCount(petTalk.getReplyCount())
-                .reacted(petTalk.isReacted())
-                .writer(Writer.fromEntity(petTalk.getWriter()))
+                .id(petTalkView.getPetTalkId())
+                .title(petTalkView.getTitle())
+                .content(petTalkView.getContent())
+                .thumbnail(petTalkView.getThumbnail())
+                .viewCount(petTalkView.getViewCount())
+                .replyCount(petTalkView.getReplyCount())
+                .reacted(petTalkView.isReacted())
+                .createdAt(petTalkView.getCreatedAt())
+                .totalEmojiCount(petTalkView.getTotalEmojiCount())
+                .writer(Writer.fromEntity(petTalkView.getWriter()))
                 .build();
     }
 
