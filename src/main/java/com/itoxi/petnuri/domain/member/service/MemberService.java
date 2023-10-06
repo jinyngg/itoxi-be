60a0b96dfc14ebc48def7be7b0979bd6f258fdb8
inyoung <charmingyear@gmail.com>
2023-10-06 15:32:34 +0900

    [#178] chore : 사용하지 않는 import문 제거

--- src/main/java/com/itoxi/petnuri/domain/member/service/MemberService.java
+++ src/main/java/com/itoxi/petnuri/domain/member/service/MemberService.java
@@ -39,7 +39,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.itoxi.petnuri.global.common.exception.type.ErrorCode.PET_GENDER_NOT_FOUND;
